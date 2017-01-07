41dbc613914c8a51bd184cc9aa1b17897c106700
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2017-01-08 05:17:03 +0530

    Converted Code to Use TS + ArrowFunctions and Added Example Test

--- consoleEx.js
+++ consoleEx.js
@@ -1,37 +1,47 @@
-(function(console){
+(function (console) {
     "use strict";
-    function getBox(width, height) {
+    var _this = this;
+    var getBox = function (width, height) {
         return {
             string: "+",
-            style: "font-size: 1px; padding: " + Math.floor(height/2) + "px " + Math.floor(width/2) + "px; line-height: " + height + "px;"
-        }
-    };
-    console.logImage = function(url, scale) {
-        scale = scale || 1;
-        var img = new Image();
-
-        img.onload = function() {
-            var dim = getBox(this.width * scale, this.height * scale);
-            console.log("%c" + dim.string, dim.style + "background: url(" + url + "); background-size: " + (this.width * scale) + "px " + (this.height * scale) + "px; color: transparent;");
+            style: "font-size: 1px; padding: " + Math.floor(height / 2) + "px " + Math.floor(width / 2) + "px; line-height: " + height + "px;"
         };
-
+    };
+    console.logImage = function (url, scale) {
+        if (scale === void 0) { scale = 1; }
+        var img = new Image();
+        img.onload = function () {
+            var dim = getBox(_this.width * scale, _this.height * scale);
+            console.log("%c" + dim.string, dim.style + "background: url(" + url + "); background-size: " + (_this.width * scale) + "px " + (_this.height * scale) + "px; color: transparent;");
+        };
         img.src = url;
     };
-    console.logSuccess = function(text) {
-        const css = 'background: green; color: white; display: block; font-size: 16px';
-        console.log('%c'+text, css);
+    console.logSuccess = function (text, color, background, size) {
+        if (color === void 0) { color = 'white'; }
+        if (background === void 0) { background = 'green'; }
+        if (size === void 0) { size = '20px'; }
+        var css = 'color: ' + color + '; background: ' + background + '; font-size: ' + size;
+        console.log('%c' + text, css);
     };
-    console.logFailure = function(text) {
-        const css = 'background: red; color: white; display: block; font-size: 16px';
-        console.log('%c'+text, css);
+    console.logFailure = function (text, color, background, size) {
+        if (color === void 0) { color = 'white'; }
+        if (background === void 0) { background = 'red'; }
+        if (size === void 0) { size = '20px'; }
+        var css = 'color: ' + color + '; background: ' + background + '; font-size: ' + size;
+        console.error('%c' + text, css);
     };
-    console.logWarning = function(text) {
-        const css = 'background: orange; color: white; display: block; font-size: 16px';
-        console.log('%c'+text, css);
+    console.logWarning = function (text, color, background, size) {
+        if (color === void 0) { color = 'white'; }
+        if (background === void 0) { background = 'orange'; }
+        if (size === void 0) { size = '20px'; }
+        var css = 'color: ' + color + '; background: ' + background + '; font-size: ' + size;
+        console.warn('%c' + text, css);
+    };
+    console.logStyled = function (text, color, background, size) {
+        if (color === void 0) { color = 'black'; }
+        if (background === void 0) { background = 'white'; }
+        if (size === void 0) { size = '20px'; }
+        var css = 'color: ' + color + '; background: ' + background + '; font-size: ' + size;
+        console.log('%c' + text, css);
     };
 })(console);
-
-console.logImage('https://i.cloudup.com/Zqeq2GhGjt-3000x3000.jpeg');
-console.logSuccess('Success');
-console.logFailure('Failure');
-console.logWarning('Warning');
